dec367bb072f165132684810328772676d801b47
shmosn <44983685+shmosn@users.noreply.github.com>
2019-02-11 09:25:21 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,59 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <PubSubClient.h> //MQTT Broker Server
+#include <iostream>
+#include<string>
+#include<sstream>
 
-const char* ssid = "YOUR WIFI SSID";
-const char* pass = "YOUR WIFI PASSWORD";
-const char* brokerUser = "USE YOUR BROKER USER MAIL"; //login info on dioty.co
-const char* brokerPass = "USE YOUR BROKER PASS";
+const char* ssid = "FRITZ!Box 6490 Cable";
+const char* pass = "honisoitquimalypense!";
+const char* brokerUser = "boxleap@gmail.com"; //login info on dioty.co
+const char* brokerPass = "32ec3f29";
 const char* broker = "mqtt.dioty.co";
-const char* inTopic = "/YOURBROKERUSERADDRESS/EspTest";
+const char* inTopic = "/boxleap@gmail.com/esptest";
+const char* outTopic = "/boxleap@gmail.com/esptestreply";
+
+const int lockPin(3);
+const int wifiIndicatorPin(19);
+const int loginToDioty(18);
+const int ledPin25(5); //pin 8 = GPIO05
+const int ledPin50(17); //pin 9 = GPIO17
+const int ledPin75(16); //pin 10 = GPIO16
+const int ledPin100(4); // pin 11 = GPIO04
+
+char messages[50];
 
 WiFiClient espClient; // NOT sure what that does
 PubSubClient client(espClient); // Nor this.., after this: reconnect function for server connection
+
+int getValueFromMessage(std::string msg, std::string id);
+int percentageToNumberOfLeds(int value);
+void lightProcessLEDs(int numberOfLeds, int ledPin1, int ledPin2, int ledPin3, int ledPin4);
+
+void identifyMessage(std::string msg){
+    // see what kind of message we receive
+    
+    std::string substring = msg.substr(0,2);
+    if(substring == "pe"){
+      const int value(getValueFromMessage(msg, "pe"));
+      const int numberOfLeds = percentageToNumberOfLeds(value);
+      lightProcessLEDs(numberOfLeds, ledPin25, ledPin50, ledPin75, ledPin100);
+
+      const char * b = (char*)value;
+      client.publish(outTopic, "Process LEDs updated to:");
+      // SEEMS TO CRASH client.publish(outTopic, b);
+    }
+    else if(msg.substr(0,4) == "lock"){
+        digitalWrite(lockPin, LOW);
+        client.publish(outTopic, "box now locked.");
+    }
+
+    else if(msg.substr(0,6) == "unlock") {
+        digitalWrite(lockPin, HIGH);
+        client.publish(outTopic, "box now unlocked.");
+    }
+
+}
 
 
 void setupWifi(){
@@ -23,9 +66,9 @@ void setupWifi(){
     delay(100);
     Serial.print("-");
     
-    digitalWrite(18, HIGH);
+    digitalWrite(loginToDioty, LOW);
     delay(100);
-    digitalWrite(18, LOW);
+    digitalWrite(loginToDioty, HIGH);
     }
 
   Serial.print("\nConnected to ");
@@ -39,15 +82,15 @@ void reconnect(){
     if(client.connect("dev0001", brokerUser, brokerPass)){
       Serial.print("\nConnected to ");
       Serial.println(broker);
-      digitalWrite(19, HIGH);
+      digitalWrite(wifiIndicatorPin, HIGH);
       client.subscribe(inTopic);
     } else {
       Serial.println("\nTrying to connect again...");
       //delay(5000); // instead: for-loop for flashing LED as long as not connected..
       for (int a = 0; a < 33; a++) {
-        digitalWrite(19, HIGH);
+        digitalWrite(wifiIndicatorPin, HIGH);
         delay(50);
-        digitalWrite(19, LOW);
+        digitalWrite(wifiIndicatorPin, LOW);
         delay(100);
       }
     }
@@ -68,37 +111,40 @@ void callback(char* topic, byte* payload, unsigned int length){ //callback funct
   message_buff[i] = '\0';
 
   String msgString = String(message_buff); // Converting message to string
-  Serial.println("Payload: " + msgString);
-  Serial.println(msgString[1]);
-  /*for(int i=0; i<length; i++){
-    Serial.print((char) payload[i]);
-  }
-  Serial.println();
-  */
-  // filter out true false
+  //Serial.println("Payload: " + msgString);
+  //Serial.println(msgString[0]);
+ 
+  // ------- MESSAGE PARSE --------
+  
+  identifyMessage(msgString.c_str());
 
-  if(msgString[0]=='t'){
-    digitalWrite(21, HIGH);
-  } else {
-    digitalWrite(21, LOW);
-  }
-} 
+  if(msgString[0]=='t' || msgString[0]=='f')
+    client.publish(outTopic, "try either 'pex', where x is a value between 0 and 100, or 'lock' / 'unlock'");
+
+}
 
 void setup() {
   Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT); //ESP32 WROOM board: blaue SMD LED
-  pinMode(18, OUTPUT);
-  pinMode(19, OUTPUT); // server connection status: flashing = connecting, on = connected
-  pinMode(21, OUTPUT); // controlled Light by MQTT message
+  pinMode(wifiIndicatorPin, OUTPUT);
+  pinMode(loginToDioty, OUTPUT); // server connection status: flashing = connecting, on = connected
+  pinMode(lockPin, OUTPUT); // controlled Light by MQTT message
+  pinMode(ledPin25, OUTPUT);
+  pinMode(ledPin50, OUTPUT);
+  pinMode(ledPin75, OUTPUT);
+  pinMode(ledPin100, OUTPUT);
+  
   setupWifi();
   client.setServer(broker, 1883);
   client.setCallback(callback);
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
   if (!client.connected()){
     reconnect();
   }
   client.loop();
+
 }
